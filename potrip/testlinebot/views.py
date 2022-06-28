2b96ce762409e72ae72ec703f7a3f34b715c33e4
jerry <james3673@gmail.com>
2022-06-28 19:51:54 +0800

    test 20220628

--- potrip/testlinebot/views.py
+++ potrip/testlinebot/views.py
@@ -1,3 +1,4 @@
+from email import message
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
@@ -6,6 +7,10 @@ from django.conf import settings
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
+
+from googlesearch import search
+from bs4 import BeautifulSoup
+
  
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
@@ -13,6 +18,8 @@ parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
  
 @csrf_exempt
 def callback(request):
+
+    keyword_bot_name = '狗狗'
  
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
@@ -26,11 +33,34 @@ def callback(request):
             return HttpResponseBadRequest()
  
         for event in events:
-            if isinstance(event, MessageEvent):  # 如果有訊息事件
+            msg_text = event.message.text
+
+            if len(msg_text) > 2:
+                keyword = msg_text[:2]
+            else:
+                keyword = ''
+
+            if isinstance(event, MessageEvent) and keyword_bot_name in keyword:  # 如果有訊息事件
+                # print(event)
                 line_bot_api.reply_message(  # 回復傳入的訊息文字
                     event.reply_token,
-                    TextSendMessage(text=event.message.text)
+                    TextSendMessage(handle_message(msg_text))
                 )
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
+
+def handle_message(msg_text):
+    
+    keyword_search = ' 魔法覺醒 Youtube'
+    print(msg_text)
+    msg_text = msg_text[2:]
+    print(msg_text)
+    query = msg_text + keyword_search
+    print(query)
+    for j in search(query, stop=1, pause=2.0): 
+        return j
+
+
+
+
